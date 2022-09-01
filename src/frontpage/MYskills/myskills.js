083945cba88d44ad39b60439687ea8002b1e3ff4
Ujjwal Singh <ujjwal15singh@gmail.com>
2022-09-01 19:27:02 +0530

    updated UI

--- src/frontpage/MYskills/myskills.js
+++ src/frontpage/MYskills/myskills.js
@@ -36,13 +36,13 @@ class Myskill extends Component{
                   <div className="col-sm-4 center"><img src="/assets/js.svg"></img><div class="label">Javascript</div></div>
                 </div>
                 <div data-aos="fade-up" className="display2">
-                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/Angular.svg"></img><div class="label">Angular 10</div></div>
+                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/redux.svg"></img><div class="label">Redux</div></div>
                   <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/react.svg"></img><div class="label">React.JS</div></div>
                   <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/node.svg"></img><div class="label">Node.Js</div></div>
                   </div>
                   <div data-aos="fade-up" className="display2">
                   <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/c.svg"></img><div class="label">C++</div></div>
-                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/mongo.svg"></img><div class="label">MongoDB</div></div>
+                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/sql.svg"></img><div class="label">SQL</div></div>
                   <div style={{textAlign:"center",width:"33vw"}}><img src="/assets/git.svg"></img><div class="label">Github</div></div>
                   </div>
                   <div data-aos="fade-up" className="display2">
